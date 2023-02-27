76ad2e2de3745cf652fb21299451d10271caa64d
johnmartinezc <john.martinez@codeimmersives.com>
2023-02-27 00:49:41 -0500

    -update

---
+++ ReactIntroForm/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { useState } from "react";
+
+import './App.css';
+
+
+
+
+function MovieForm() { 
+  return (
+    <div className="MovieForm">
+      <header  className="App-header">
+        <h1>Movie Form</h1>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+
+class MovieForms extends Component{
+  render(){
+    return (
+      <div className="App">
+      <h1>Hello, React !</h1>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default MovieForm;
